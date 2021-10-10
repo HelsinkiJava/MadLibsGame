61a0006258e902f3ebc162bcc2e7e6ccc9e724ef
HelsinkiJava <gmaki@live.com>
2021-10-10 00:02:06 -0700

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/MadLibsGame.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MadLibsGame.dir/main.c.obj"
+  "MadLibsGame.exe"
+  "MadLibsGame.exe.manifest"
+  "MadLibsGame.pdb"
+  "libMadLibsGame.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/MadLibsGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    printf("Roses are {color}\n");
+    printf("{pluralNoun} are blue\n");
+    printf("I love {person}\n");
+    return 0;
+}

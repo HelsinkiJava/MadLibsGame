edc3666e1a43e7e5938f087be9a945dacd198855
HelsinkiJava <gmaki@live.com>
2021-10-10 00:19:45 -0700

    Initial commit

--- main.c
+++ main.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 
 int main() {
-    printf("Roses are {color}\n");
-    printf("{pluralNoun} are blue\n");
-    printf("I love {person}\n");
+    char color[20];
+    char noun[20];
+    char person[20];
+
+    printf("Enter a color: ");
+    scanf(" %s", color);
+    printf("Enter a noun: ");
+    scanf(" %s", noun);
+    // We use the next line to eat up the newline character the scanf() leaves in the buffer
+    getchar();
+    printf("Enter a person: ");
+    fgets(person, 20, stdin);
+
+    printf("Roses are %s\n", color);
+    printf("%s are blue\n", noun);
+    printf("I love %s\n", person);
     return 0;
 }
